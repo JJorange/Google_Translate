6c0e6961a2d25e70b26ea4a1118bfdd9bddf043d
JJorange <31954831+JJorange@users.noreply.github.com>
2019-10-21 22:08:09 +0800

    add a python file for google_translate

---
+++ test.py
@@ -0,0 +1,277 @@
+import execjs
+import requests
+import random
+import json
+import socket
+import hashlib
+from multiprocessing import Process
+from urllib.parse import unquote
+
+class Py4Js():
+    def __init__(self):
+        self.ctx = execjs.compile("""
+        function TL(a) {
+        var k = "";
+        var b = 406644;
+        var b1 = 3293161072;
+
+        var jd = ".";
+        var $b = "+-a^+6";
+        var Zb = "+-3^+b+-f";
+
+        for (var e = [], f = 0, g = 0; g < a.length; g++) {
+            var m = a.charCodeAt(g);
+            128 > m ? e[f++] = m : (2048 > m ? e[f++] = m >> 6 | 192 : (55296 == (m & 64512) && g + 1 < a.length && 56320 == (a.charCodeAt(g + 1) & 64512) ? (m = 65536 + ((m & 1023) << 10) + (a.charCodeAt(++g) & 1023),
+            e[f++] = m >> 18 | 240,
+            e[f++] = m >> 12 & 63 | 128) : e[f++] = m >> 12 | 224,
+            e[f++] = m >> 6 & 63 | 128),
+            e[f++] = m & 63 | 128)
+        }
+        a = b;
+        for (f = 0; f < e.length; f++) a += e[f],
+        a = RL(a, $b);
+        a = RL(a, Zb);
+        a ^= b1 || 0;
+        0 > a && (a = (a & 2147483647) + 2147483648);
+        a %= 1E6;
+        return a.toString() + jd + (a ^ b)
+    };
+
+    function RL(a, b) {
+        var t = "a";
+        var Yb = "+";
+        for (var c = 0; c < b.length - 2; c += 3) {
+            var d = b.charAt(c + 2),
+            d = d >= t ? d.charCodeAt(0) - 87 : Number(d),
+            d = b.charAt(c + 1) == Yb ? a >>> d: a << d;
+            a = b.charAt(c) == Yb ? a + d & 4294967295 : a ^ d
+        }
+        return a
+    }
+    """)
+
+    def getTk(self, text):
+        print(text)
+        return self.ctx.call("TL", text)
+
+def GetContentfFlile(filename):
+    file = open(filename,"r")
+    content_list = file.read().splitlines()
+    file.close()
+    return content_list
+
+def GetRequestUrl(content):
+    py4js = Py4Js()
+    tk = py4js.getTk(content)
+    print(content)
+    print("tk:" + tk)
+    url = "https://translate.google.cn/translate_a/"\
+          "single?client=webapp&sl=zh-CN&tl=en&hl=zh-CN&dt=at&dt=bd&dt=ex&dt=ld&dt=md&dt=qca&dt=rw&dt=rm&dt=ss&dt=t&source=bh&ssel=0&tsel=0&kc=1&tk=%s&q=%s" % (tk,content)
+    return url
+
+def GetProxy(proxy_file):
+    file = open(proxy_file)
+    proxy_list = file.read().splitlines()
+    file.close()
+    return proxy_list
+
+def GetUserAgent():
+    '''
+    功能：随机获取HTTP_User_Agent
+    '''
+    user_agents=[
+    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; AcooBrowser; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
+    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; Acoo Browser; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; .NET CLR 3.0.04506)",
+    "Mozilla/4.0 (compatible; MSIE 7.0; AOL 9.5; AOLBuild 4337.35; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
+    "Mozilla/5.0 (Windows; U; MSIE 9.0; Windows NT 9.0; en-US)",
+    "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 2.0.50727; Media Center PC 6.0)",
+    "Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 1.0.3705; .NET CLR 1.1.4322)",
+    "Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 5.2; .NET CLR 1.1.4322; .NET CLR 2.0.50727; InfoPath.2; .NET CLR 3.0.04506.30)",
+    "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN) AppleWebKit/523.15 (KHTML, like Gecko, Safari/419.3) Arora/0.3 (Change: 287 c9dfb30)",
+    "Mozilla/5.0 (X11; U; Linux; en-US) AppleWebKit/527+ (KHTML, like Gecko, Safari/419.3) Arora/0.6",
+    "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.2pre) Gecko/20070215 K-Ninja/2.1.1",
+    "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.9) Gecko/20080705 Firefox/3.0 Kapiko/3.0",
+    "Mozilla/5.0 (X11; Linux i686; U;) Gecko/20070322 Kazehakase/0.4.5",
+    "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.8) Gecko Fedora/1.9.0.8-1.fc10 Kazehakase/0.5.6",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/535.20 (KHTML, like Gecko) Chrome/19.0.1036.7 Safari/535.20",
+    "Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; fr) Presto/2.9.168 Version/11.52",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.11 TaoBrowser/2.0 Safari/536.11",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.71 Safari/537.1 LBBROWSER",
+    "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E; LBBROWSER)",
+    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E; LBBROWSER)",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.84 Safari/535.11 LBBROWSER",
+    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E)",
+    "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E; QQBrowser/7.0.3698.400)",
+    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E)",
+    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; SV1; QQDownload 732; .NET4.0C; .NET4.0E; 360SE)",
+    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E)",
+    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E)",
+    "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.89 Safari/537.1",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.89 Safari/537.1",
+    "Mozilla/5.0 (iPad; U; CPU OS 4_2_1 like Mac OS X; zh-cn) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8C148 Safari/6533.18.5",
+    "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:2.0b13pre) Gecko/20110307 Firefox/4.0b13pre",
+    "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:16.0) Gecko/20100101 Firefox/16.0",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11",
+    "Mozilla/5.0 (X11; U; Linux x86_64; zh-CN; rv:1.9.2.10) Gecko/20100922 Ubuntu/10.10 (maverick) Firefox/3.6.10"
+    ]
+    user_agent = random.choice(user_agents)
+    return user_agent
+
+def SendRequest(request_url,proxy_list):
+    for ip_port in proxy_list:
+        ip_port = random.choice(proxy_list)
+        if ip_port == 0:
+            continue
+        user_agent = GetUserAgent()
+        header = {
+            "User-Agent":user_agent,
+        }
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (X11; U; Linux x86_64; zh-CN; rv:1.9.2.10) Gecko/20100922 Ubuntu/10.10 (maverick) Firefox/3.6.10',
+            'Connection': 'keep-alive'
+        }
+        #ip = "91.187.113.205:53281"
+
+        proxy = {
+            'http:':ip_port,
+            # 'https': 'https://' + proxy,
+        }
+        #url = "https://translate.google.cn/translate_a/single?client=webapp&sl=zh-CN&tl=en&hl=zh-CN&dt=at&dt=bd&dt=ex&dt=ld&dt=md&dt=qca&dt=rw&dt=rm&dt=ss&dt=t&source=bh&ssel=0&tsel=0&kc=1&tk=690327.834787&q=早餐"
+        print(request_url)
+        print(proxy)
+        r = requests.get(request_url, headers=header, proxy = proxy)
+        print("post:::")
+        print(r.status_code)
+        print("\t")
+        print(r.content)
+        if r.status_code != 200:
+            ip_port = 0
+            continue
+        return r.content
+
+def write_to_file(result_file,content,result):
+    result_file.write(content + ":")
+    result_file.write(result)
+    result_file.write("\n")
+    result_file.flush()
+
+
+def local_translate():
+    filename = "translate_requst.txt"
+    proxies_file = "usable_ip.pkl2"
+    content_list = GetContentfFlile(filename)
+    result_file = open("translate_result.txt","w",encoding='utf-8')
+    for content in content_list:
+        request_url = GetRequestUrl(content)
+        proxy_list = GetProxy(proxies_file)
+        response = SendRequest(request_url,proxy_list)
+        if response != None:
+            #result_file.write(content + ":")
+            parse_json = json.loads(str(response,encoding = "utf-8"))
+            result = parse_json[0][0][0]
+            write_to_file(result_file,content,result)
+    result_file.close()
+
+def zh2en(src_content):
+    result = str()
+    request_url = GetRequestUrl(src_content)
+    proxy_list = GetProxy("usable_ip.pkl")
+    response = SendRequest(request_url, proxy_list)
+    if response != None:
+        # result_file.write(content + ":")
+        parse_json = json.loads(str(response, encoding="utf-8"))
+        result += parse_json[0][0][0]
+    return result
+
+def zh2en_interface(request):
+    user_agent = GetUserAgent()
+    header = {
+        'Accpet': 'application/json, text/javascript, */*; 1=0.01',
+        "Content-Type": 'application/x-www-form-urlencoded; charset=UTF-8',
+        "Origin": 'http://speech.ths8.com:6080',
+        "Referer": 'http://speech.ths8.com:6080/translate/index.html',
+        "User-Agent": user_agent,
+    }
+    data2 = "text=%s&appId=1&appKey=1&type=zh2en"%(request)
+
+    #http://speech.ths8.com:10020/translateConnection/get/result?text=早餐&appId=1&appKey=1&type=zh2en
+    url = "http://speech.ths8.com:10020/translateConnection/translate.do"
+    #url_google = "https://translate.google.cn/translate_a/single?client=webapp&sl=zh-CN&tl=en&hl=zh-CN&dt=at&dt=bd&dt=ex&dt=ld&dt=md&dt=qca&dt=rw&dt=rm&dt=ss&dt=t&source=bh&ssel=0&tsel=0&kc=1&tk=690327.834787&q=早餐"
+    print (url)
+    #response = requests.get(url)
+    response = requests.post(url, data=data2.encode('utf-8'), headers=header)
+    if request != None:
+        print(response.content)
+        parse_json = json.loads(str(response.content,encoding='utf-8'))
+        print("md5::::")
+        a = hashlib.md5()
+        a.update(response.content)
+        print(a.hexdigest())
+        result = parse_json['data']
+        print (result[:-1])
+        return result[:-1]
+    return None
+
+def zh2en_baidu(request):
+    #url = http://api.fanyi.baidu.com/api/trans/vip/translate?q=apple&from=en&to=zh&appid=2015063000000001&salt=1435660288&sign=f89f9594663708c1605f3d736d01d2d4
+    url = 1
+
+
+def net_translate(request,translate_type):
+    if request == None:
+        return None
+    return zh2en_interface(request)
+    #return zh2en(request)
+
+def parse_reqest(request_data):
+    data_list = str(request_data,encoding='gbk').split()
+    print(request_data)
+    print("::")
+    print(data_list)
+    if "GET" == data_list[0]:
+        content = data_list[1]
+        if content.split("?")[0] == "/translate":
+            type = content.split("&")[0].split("=")[1]
+            input = content.split("&")[1][6:]
+            print("ok")
+            a = bytes(input,encoding='gbk')
+            string = str(a, 'gbk')
+            result =  net_translate(unquote(input),type)
+            return unquote(str(input)) + ":" + result
+        else:
+            return None
+    else:
+        return "not support port request！"
+def handle_client(client_socket):
+    """
+    处理客户端请求
+    """
+    request_data = client_socket.recv(2048)
+    print("request data:", request_data)
+    #解析收到的数据
+    result = parse_reqest(request_data)
+    # 构造响应数据
+    response_start_line = "HTTP/1.1 200 OK\r\n"
+    response_headers = "Server: My server\r\n"
+    print(result)
+    response_body = str(result)
+    response = response_start_line + response_headers + "\r\n" + response_body
+
+    # 向客户端返回响应数据
+    client_socket.send(bytes(response, "utf-8"))
+
+    # 关闭客户端连接
+    client_socket.close()
+
+if __name__ == "__main__":
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(("", 8001))
+    server_socket.listen(128)
+
+    while True:
+        client_socket, client_address = server_socket.accept()
+        print("[%s, %s]用户连接上了" % client_address)
+        handle_client_process = Process(target=handle_client, args=(client_socket,))
+        handle_client_process.start()
+        client_socket.close()
